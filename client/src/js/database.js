98dc179a6a05a5ed765f62d3dd69af843e3ea041
Eli Baynes <sbaynes1994@gmail.com>
2023-03-28 22:14:59 -0500

    connection logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,22 +12,35 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   // connect to the database and version
+  const JATEdb = await openDB('jate', 1);
   // create a transaction and privelages
+  const tx = JATEdb.transaction('jate', 'readwrite');
   // open the object store
+  const store = tx.objectStore('jate');
   // add the content to the database
+  const req = store.add(content);
   // confirm the content was added
+  const result = await req;
+  // return the result
+  console.log('content added to database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   // connect to the database and version
+  const JATEdb = await openDB('jate', 1);
   // create a transaction and privelages
+  const tx = JATEdb.transaction('jate', 'readonly');
   // open the object store
+  const store = tx.objectStore('jate');
   // get all the content from the database
+  const content = await store.getAll();
   // return the content
+  const result = await content;
+  console.log('content retrieved from database', result);
+  // return the value of the result
+  return result.value;
 }
 
 initdb();
